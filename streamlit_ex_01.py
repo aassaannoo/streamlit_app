bfd096e7b9d536e8548c202beba97dc642dae6ad
Asano Hiroki <t125005@ed.sus.ac.jp>
2026-01-26 11:43:55 +0900

    readme&csv

--- streamlit_ex_01.py
+++ streamlit_ex_01.py
@@ -1,1 +1,9 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+st.title('広告費と売り上げ')
+data = pd.read_csv('zmy2020a.csv')
+df = pd.DataFrame(data)
+
