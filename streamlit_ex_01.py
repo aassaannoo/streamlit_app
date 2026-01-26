de76d697999c05195f41f33168da9f2c4c1a9333
Asano Hiroki <t125005@ed.sus.ac.jp>
2026-01-26 12:09:47 +0900

    requirements

--- streamlit_ex_01.py
+++ streamlit_ex_01.py
@@ -3,7 +3,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-st.title('広告費と売り上げ')
-data = pd.read_csv('zmy2020a.csv')
+st.title('消費者物価指数（CPI）の比較')
+data = pd.read_csv('zmy2020a.csv', encoding='cp932')
 df = pd.DataFrame(data)
 
+
+with st.sidebar:
+    kikan = st.select_slider("期間の範囲を選択してください",
+                             )
